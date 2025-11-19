db2f69c39d0e03f3893ae4fce34b949dc5c66b7b
Vlad <vladknazev312@gmail.com>
2025-11-19 18:40:48 +0600

    change script

--- script.js
+++ script.js
@@ -1,3 +1,168 @@
+// HomeWork_7
+
+const orders = [
+  {
+    id: 1,
+    customer: "Jhon",
+    items: [
+      { name: "Ноутбук",  price: 50000, quantity: 1 },
+      { name: "Мышь",     price: 1500,  quantity: 2 }
+    ]
+  },
+  {
+    id: 2,
+    customer: "Alan",
+    items: [
+      { name: "Телефон", price: 30000, quantity: 1 },
+      { name: "Чехол",   price: 1000,  quantity: 3 }
+    ]
+  },
+  {
+    id: 3,
+    customer: "Jane",
+    items: [
+      { name: "Клавиатура", price: 2500,  quantity: 1 },
+      { name: "Монитор",    price: 12000, quantity: 1 }
+    ]
+  }
+];
+
+console.log("Сумма заказов: ");
+for (var i = 0; i < orders.length; i++) {
+  var order = orders[i];
+  var orderSum = 0;
+  
+  for (var j = 0; j < order.items.length; j++) {
+    var item = order.items[j];
+    orderSum += item.price * item.quantity;
+  }
+  
+  console.log(`Заказ ${order.id} (${order.customer}): ${orderSum} $`);
+}
+
+
+
+var TotalInCome = 0
+for (var i = 0; i < orders.length; i++) {
+  var order = orders[i]
+  
+  for (var j = 0; j < order.items.length; j++) {
+    var item = order.items[j]
+    TotalInCome += item.price * item.quantity
+  }
+}
+console.log("\nСумма дохода = " + TotalInCome + " $ ")
+
+
+
+var maxSum = 0
+var MostExpensive = ""
+
+for (var i = 0; i < orders.length; i++) {
+  var order = orders[i]
+  var orderSum = 0
+  
+  for (var j = 0; j < order.items.length; j++) {
+    var item = order.items[j]
+    orderSum += item.price * item.quantity
+  }
+  if (orderSum > maxSum) {
+    maxSum = orderSum
+    MostExpensive = order.customer
+  }
+}
+console.log("Самый дорогой заказ = " + MostExpensive + " (" + maxSum + " $ )")
+
+
+
+//Дополнительное задание:
+
+console.log("\nВсе проданные товары: ")
+var allProducts = {}
+
+for (var i = 0; i < orders.length; i++) {
+  var order = orders[i]
+  
+  for (var j = 0; j < order.items.length; j++) {
+    var item = order.items[j]
+    var NameProduct = item.name
+    if (allProducts[NameProduct]) {
+      allProducts[NameProduct] += item.quantity
+    } else {
+      allProducts[NameProduct] = item.quantity
+    }
+  }
+}
+for (var NameProduct in allProducts) {
+  console.log(NameProduct + " = " + allProducts[NameProduct] + " (шт)")
+}
+
+
+
+var PopularProduct = ""
+var maxQuantity = 0
+
+for (var NameProduct in allProducts) {
+  if (allProducts[NameProduct] > maxQuantity) {
+    maxQuantity = allProducts[NameProduct]
+    PopularProduct = NameProduct
+  }
+}
+console.log("\nСамый популярный товар = " + PopularProduct + " (" + maxQuantity + " (шт))")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lesson_7 Циклы и Массивы
+
+
 // var login = "user"
 // var password = "qwerty"
 // var email = "email@gmail.com"
